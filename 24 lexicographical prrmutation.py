9759189bb1ddde685e0a0206d00fb335f46f9767
jchristy40 <49081710+jchristy40@users.noreply.github.com>
2019-06-12 16:09:02 -0500

    24 lexicographical permutations
    - note : I had no idea how to generate permutations before looking at this problem. Credit to Nayuki.io for the first part of the solution 
    
    A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
    
    012   021   102   120   201   210
    
    What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?

---
+++ 24 lexicographical prrmutation.py
@@ -0,0 +1,35 @@
+def next_permutation(arr):
+    # Find non-increasing suffix
+    i = len(arr) - 1
+    while i > 0 and arr[i - 1] >= arr[i]:
+        i -= 1
+    if i <= 0:
+        return False
+    
+    # Find successor to pivot
+    j = len(arr) - 1
+    while arr[j] <= arr[i - 1]:
+        j -= 1
+    arr[i - 1], arr[j] = arr[j], arr[i - 1]
+    
+    # Reverse suffix
+    arr[i : ] = arr[len(arr) - 1 : i - 1 : -1]
+    
+    return True
+    
+
+arr=[0,1,2,3,4,5,6,7,8,9]
+x=1
+formatted=''
+while x <1000000:
+	next_permutation(arr)
+	x=x+1
+else:
+	for i in arr:
+		i=str(i)
+		formatted=formatted +i
+	else:
+		print(formatted)
+	
+
+print(next_permutation(arr))
